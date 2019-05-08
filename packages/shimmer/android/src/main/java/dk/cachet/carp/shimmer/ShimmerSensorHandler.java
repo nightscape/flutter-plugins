146bc62bf6f6f6526cb14bc520e8f192da7996e0
Martin Mauch <martin.mauch@gmail.com>
2019-05-08 22:43:17 +0200

    Use ShimmerBluetoothManagerAndroid for coordinating the connection process

--- packages/shimmer/android/src/main/java/dk/cachet/carp/shimmer/ShimmerSensorHandler.java
+++ packages/shimmer/android/src/main/java/dk/cachet/carp/shimmer/ShimmerSensorHandler.java
@@ -21,12 +21,30 @@ import com.shimmerresearch.driver.Configuration;
 import com.shimmerresearch.driver.FormatCluster;
 import com.shimmerresearch.driver.ObjectCluster;
 
+import android.view.View;
+import android.widget.TextView;
+
+import com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog;
+import com.shimmerresearch.android.guiUtilities.ShimmerDialogConfigurations;
+import com.shimmerresearch.android.manager.ShimmerBluetoothManagerAndroid;
+import com.shimmerresearch.driver.CallbackObject;
+import com.shimmerresearch.driver.ShimmerDevice;
+import com.shimmerresearch.managers.bluetoothManager.ShimmerBluetoothManager;
+
+import java.io.IOException;
+import java.util.Collection;
+
+import static com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog.EXTRA_DEVICE_ADDRESS;
+import static com.shimmerresearch.android.guiUtilities.ShimmerBluetoothDialog.REQUEST_CONNECT_SHIMMER;
+
 public class ShimmerSensorHandler implements SensorHandler
 {
     private Shimmer sensor;
     private final Activity activity;
     private final EventChannel.EventSink eventSink;
     private final String macAddress;
+    final static String LOG_TAG = "BluetoothManagerExample";
+    private ShimmerBluetoothManagerAndroid btManager;
 
     public ShimmerSensorHandler(Activity activity, EventChannel.EventSink eventSink, String macAddress)
     {
@@ -35,7 +53,113 @@ public class ShimmerSensorHandler implements SensorHandler
         this.macAddress = macAddress;
         Log.d("PermissionActivity", this.eventSink.toString());
     }
+    private ShimmerDevice shimmerDevice;
+    public class ShimmerHandler2 extends Handler {
 
+        private ShimmerBluetoothManager btManager;
+        private final EventChannel.EventSink eventSink;
+
+        public ShimmerHandler2(EventChannel.EventSink eventSink)
+        {
+            this.eventSink = eventSink;
+        }
+
+        @Override
+        public void handleMessage(Message msg) {
+
+            switch (msg.what) {
+                case ShimmerBluetooth.MSG_IDENTIFIER_DATA_PACKET:
+                    if ((msg.obj instanceof ObjectCluster)) {
+                        /*
+                        ObjectCluster objectCluster = (ObjectCluster) msg.obj;
+
+                        //Retrieve all possible formats for the current sensor device:
+                        Collection<FormatCluster> allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.TIMESTAMP);
+                        FormatCluster timeStampCluster = ((FormatCluster) ObjectCluster.returnFormatCluster(allFormats, "CAL"));
+                        double timeStampData = timeStampCluster.mData;
+                        Log.i(LOG_TAG, "Time Stamp: " + timeStampData);
+                        allFormats = objectCluster.getCollectionOfFormatClusters(Configuration.Shimmer3.ObjectClusterSensorName.ACCEL_LN_X);
+                        FormatCluster accelXCluster = ((FormatCluster) ObjectCluster.returnFormatCluster(allFormats, "CAL"));
+                        if (accelXCluster != null) {
+                            double accelXData = accelXCluster.mData;
+                            Log.i(LOG_TAG, "Accel LN X: " + accelXData);
+                        }
+                         */
+                        Map<String, Object> m = new HashMap<String, Object>();
+                        ObjectCluster obj = (ObjectCluster) msg.obj;
+                        if (obj != null)
+                        {
+                            m.put("timestamp", obj.mSystemTimeStamp);
+                            m.put("sensor", obj.getShimmerName());
+                            for (Map.Entry<String, FormatCluster> entry : obj.mPropertyCluster.entries())
+                            {
+                                m.put(entry.getKey(), entry.getValue().mData);
+                            }
+                        }
+                        eventSink.success(m);
+
+                    }
+                    break;
+                case Shimmer.MESSAGE_TOAST:
+                    /** Toast messages sent from {@link Shimmer} are received here. E.g. device xxxx now streaming.
+                     *  Note that display of these Toast messages is done automatically in the Handler in {@link com.shimmerresearch.android.shimmerService.ShimmerService} */
+
+                    //Toast.makeText(getApplicationContext(), msg.getData().getString(Shimmer.TOAST), Toast.LENGTH_SHORT).show();
+                    break;
+                case ShimmerBluetooth.MSG_IDENTIFIER_STATE_CHANGE:
+                    ShimmerBluetooth.BT_STATE state = null;
+                    String macAddress = "";
+
+                    if (msg.obj instanceof ObjectCluster) {
+                        state = ((ObjectCluster) msg.obj).mState;
+                        macAddress = ((ObjectCluster) msg.obj).getMacAddress();
+                    } else if (msg.obj instanceof CallbackObject) {
+                        state = ((CallbackObject) msg.obj).mState;
+                        macAddress = ((CallbackObject) msg.obj).mBluetoothAddress;
+                    }
+
+                    Log.d(LOG_TAG, "Shimmer state changed! Shimmer = " + macAddress + ", new state = " + state);
+
+                    switch (state) {
+                        case CONNECTED:
+                            Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now CONNECTED");
+                            shimmerDevice = btManager.getShimmerDeviceBtConnectedFromMac(macAddress); // TODO: Was (shimmerBtAdd);
+                            if (shimmerDevice != null) {
+                                Log.i(LOG_TAG, "Got the ShimmerDevice!");
+                                shimmerDevice.startStreaming();
+                            } else {
+                                Log.i(LOG_TAG, "ShimmerDevice returned is NULL!");
+                            }
+                            break;
+                        case CONNECTING:
+                            Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is CONNECTING");
+                            break;
+                        case STREAMING:
+                            Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now STREAMING");
+                            break;
+                        case STREAMING_AND_SDLOGGING:
+                            Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now STREAMING AND LOGGING");
+                            break;
+                        case SDLOGGING:
+                            Log.i(LOG_TAG, "Shimmer [" + macAddress + "] is now SDLOGGING");
+                            break;
+                        case DISCONNECTED:
+                            Log.i(LOG_TAG, "Shimmer [" + macAddress + "] has been DISCONNECTED");
+                            break;
+                    }
+                    break;
+            }
+
+            super.handleMessage(msg);
+        }
+
+        public void setBtManager(ShimmerBluetoothManager btManager)
+        {
+            this.btManager = btManager;
+        }
+    };
+
+    /*
     public static class ShimmerHandler extends Handler
     {
         private final EventChannel.EventSink eventSink;
@@ -93,6 +217,8 @@ public class ShimmerSensorHandler implements SensorHandler
     }
 
     ;
+    
+     */
 
     @Override
     public void startService()
@@ -100,10 +226,15 @@ public class ShimmerSensorHandler implements SensorHandler
         try
         {
             // The Handler that gets information back from the BluetoothChatService
-            Handler mHandler = new ShimmerHandler(eventSink);
-            sensor = new Shimmer(mHandler);
-            sensor.connect(macAddress, "default");
-            sensor.startStreaming();
+            //Handler mHandler = new ShimmerHandler(eventSink);
+            //sensor = new Shimmer(mHandler);
+            ShimmerHandler2 handler = new ShimmerHandler2(eventSink);
+            btManager = new ShimmerBluetoothManagerAndroid(activity, handler);
+            handler.setBtManager(btManager);
+
+            btManager.connectShimmerThroughBTAddress(macAddress);
+            //sensor.connect(macAddress, "default");
+            //sensor.startStreaming();
         } catch (Exception e)
         {
             throw new RuntimeException(e);
@@ -114,11 +245,27 @@ public class ShimmerSensorHandler implements SensorHandler
     @Override
     public void stopService()
     {
-        if (sensor != null)
+        /*if (sensor != null)
         {
             sensor.stopStreaming();
             sensor.stop();
         }
+         */
+        //Disconnect the Shimmer device when app is stopped
+        if (shimmerDevice != null) {
+            if (shimmerDevice.isSDLogging()) {
+                shimmerDevice.stopSDLogging();
+                Log.d(LOG_TAG, "Stopped Shimmer Logging");
+            } else if (shimmerDevice.isStreaming()) {
+                shimmerDevice.stopStreaming();
+                Log.d(LOG_TAG, "Stopped Shimmer Streaming");
+            } else {
+                shimmerDevice.stopStreamingAndLogging();
+                Log.d(LOG_TAG, "Stopped Shimmer Streaming and Logging");
+            }
+        }
+        btManager.disconnectAllDevices();
+        Log.i(LOG_TAG, "Shimmer DISCONNECTED");
     }
 
     public static class LogAndStreamExample extends Activity
@@ -243,4 +390,164 @@ public class ShimmerSensorHandler implements SensorHandler
 
     }
 
+
+
+    /**
+     * This example demonstrates the use of the {@link ShimmerBluetoothManagerAndroid} to:
+     * <ul>
+     * <li>Connect to a Shimmer device</li>
+     * <li>Stream data from the Shimmer device</li>
+     * <li>Enable and disable sensors</li>
+     * <li>Modify individual sensor configurations</li>
+     * </ul>
+     */
+    public class MainActivity extends Activity {
+
+        ShimmerBluetoothManagerAndroid btManager;
+        ShimmerDevice shimmerDevice;
+        String shimmerBtAdd = "00:00:00:00:00:00";  //Put the address of the Shimmer device you want to connect here
+
+        TextView textView;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            try {
+                ShimmerHandler2 handler = new ShimmerHandler2(eventSink);
+                btManager = new ShimmerBluetoothManagerAndroid(this, handler);
+                handler.setBtManager(btManager);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Couldn't create ShimmerBluetoothManagerAndroid. Error: " + e);
+            }
+
+        }
+
+        @Override
+        protected void onStart() {
+            //Connect the Shimmer using its Bluetooth Address
+            try {
+                btManager.connectShimmerThroughBTAddress(shimmerBtAdd);
+            } catch (Exception e) {
+                Log.e(LOG_TAG, "Error. Shimmer device not paired or Bluetooth is not enabled");
+                Toast.makeText(this, "Error. Shimmer device not paired or Bluetooth is not enabled. " +
+                        "Please close the app and pair or enable Bluetooth", Toast.LENGTH_LONG).show();
+            }
+            super.onStart();
+        }
+
+        @Override
+        protected void onStop() {
+            //Disconnect the Shimmer device when app is stopped
+            if (shimmerDevice != null) {
+                if (shimmerDevice.isSDLogging()) {
+                    shimmerDevice.stopSDLogging();
+                    Log.d(LOG_TAG, "Stopped Shimmer Logging");
+                } else if (shimmerDevice.isStreaming()) {
+                    shimmerDevice.stopStreaming();
+                    Log.d(LOG_TAG, "Stopped Shimmer Streaming");
+                } else {
+                    shimmerDevice.stopStreamingAndLogging();
+                    Log.d(LOG_TAG, "Stopped Shimmer Streaming and Logging");
+                }
+            }
+            btManager.disconnectAllDevices();
+            Log.i(LOG_TAG, "Shimmer DISCONNECTED");
+            super.onStop();
+        }
+
+        /**
+         * Messages from the Shimmer device including sensor data are received here
+         */
+        public void stopStreaming(View v) {
+            shimmerDevice.stopStreaming();
+        }
+
+        public void startStreaming(View v) {
+            shimmerDevice.startStreaming();
+        }
+
+        /**
+         * Called when the configurations button is clicked
+         *
+         * @param v
+         */
+        public void openConfigMenu(View v) {
+            if (shimmerDevice != null) {
+                if (!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
+                    ShimmerDialogConfigurations.buildShimmerConfigOptions(shimmerDevice, MainActivity.this, btManager);
+                } else {
+                    Log.e(LOG_TAG, "Cannot open menu! Shimmer device is STREAMING AND/OR LOGGING");
+                    Toast.makeText(MainActivity.this, "Cannot open menu! Shimmer device is STREAMING AND/OR LOGGING", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Log.e(LOG_TAG, "Cannot open menu! Shimmer device is not connected");
+                Toast.makeText(MainActivity.this, "Cannot open menu! Shimmer device is not connected", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        /**
+         * Called when the menu button is clicked
+         *
+         * @param v
+         * @throws IOException
+         */
+        public void openMenu(View v) throws IOException {
+
+            if (shimmerDevice != null) {
+                if (!shimmerDevice.isStreaming() && !shimmerDevice.isSDLogging()) {
+                    //ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, MainActivity.this);
+                    ShimmerDialogConfigurations.buildShimmerSensorEnableDetails(shimmerDevice, MainActivity.this, btManager);
+                } else {
+                    Log.e(LOG_TAG, "Cannot open menu! Shimmer device is STREAMING AND/OR LOGGING");
+                    Toast.makeText(MainActivity.this, "Cannot open menu! Shimmer device is STREAMING AND/OR LOGGING", Toast.LENGTH_SHORT).show();
+                }
+            } else {
+                Log.e(LOG_TAG, "Cannot open menu! Shimmer device is not connected");
+                Toast.makeText(MainActivity.this, "Cannot open menu! Shimmer device is not connected", Toast.LENGTH_SHORT).show();
+            }
+        }
+
+        /**
+         * Called when the connect button is clicked
+         *
+         * @param v
+         */
+        public void connectDevice(View v) {
+            Intent intent = new Intent(getApplicationContext(), ShimmerBluetoothDialog.class);
+            startActivityForResult(intent, ShimmerBluetoothDialog.REQUEST_CONNECT_SHIMMER);
+        }
+
+        public void startSDLogging(View v) {
+            ((ShimmerBluetooth) shimmerDevice).writeConfigTime(System.currentTimeMillis());
+            shimmerDevice.startSDLogging();
+        }
+
+        public void stopSDLogging(View v) {
+            shimmerDevice.stopSDLogging();
+        }
+
+
+        /**
+         * Get the result from the paired devices dialog
+         *
+         * @param requestCode
+         * @param resultCode
+         * @param data
+         */
+        @Override
+        protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+            if (requestCode == REQUEST_CONNECT_SHIMMER) {
+                if (resultCode == Activity.RESULT_OK) {
+                    btManager.disconnectAllDevices();   //Disconnect all devices first
+                    //Get the Bluetooth mac address of the selected device:
+                    String macAdd = data.getStringExtra(EXTRA_DEVICE_ADDRESS);
+                    btManager.connectShimmerThroughBTAddress(macAdd);   //Connect to the selected device
+                    shimmerBtAdd = macAdd;
+                }
+
+            }
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
 }
